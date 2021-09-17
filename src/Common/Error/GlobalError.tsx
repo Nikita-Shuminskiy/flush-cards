f88ac272026cc4521b5a897d475f17cc24a633ee
Nick <shuminskiy.nik@mail.ru>
2021-09-17 20:01:35 +0300

    pages

---
+++ src/Common/Error/GlobalError.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import errorPhoto from '../../Images/1548761996_2019-01-29_141751.jpg'
+
+
+export const GlobalError = () => {
+  return (
+    <div>
+      <img src={errorPhoto} alt="error"/>
+    </div>
+  );
+}
---
+++ src/Pages/PasswordRecovery/PasswordRecovery.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const PasswordRecovery = () => {
+  return (
+    <div> Login </div>
+  );
+}
