78691d50a50ae0345fb0eb04a6252f8b056cfb59
Nick <shuminskiy.nik@mail.ru>
2021-09-18 22:49:31 +0300

    Refactoring Registration/Dal

--- src/Dal/Api.ts
+++ src/Dal/Api.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import {RegistrationFormType} from '../Pages/Registration/Registration';
 
 const createInstance = axios.create({
-    /*  url: "https://neko-back.herokuapp.com/2.0"*/
-    baseURL: 'http://localhost:7542/2.0/'
+    /*  baseURL: "https://neko-back.herokuapp.com/2.0"*/
+    baseURL: 'http://localhost:7542/2.0/',
+    withCredentials: true
 })
 
 
@@ -19,25 +20,17 @@ export const registerApi = {
     },
 }
 
-
-const registerApi1 = {}
-
-
-const registerApi2 = {}
-
-
-const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
-    withCredentials: true,
-})
-
 export const api = {
     inLogin(email: string, password: string, rememberMe: boolean) {
-        const promise = instance.post('auth/login', {email, password, rememberMe})
-        return promise
+       return createInstance.post('auth/login', {email, password, rememberMe})
+
     }
 }
 
+
+
+
+//type
 type ResponseSetNewPassword = {
     info: string
     error: string
