41dafdedde987d507dc4b8df1fffcb983f3a98a5
vetero4ok <vetero4ok@gmail.com>
2021-09-18 12:17:42 +0300

    created password recovery UI components and validation

--- src/Pages/PasswordRecovery/PasswordRecovery.tsx
+++ src/Pages/PasswordRecovery/PasswordRecovery.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import {NavLink} from 'react-router-dom';
+import s from './PasswordRecovery.module.css'
 
 
 export const PasswordRecovery = () => {
-  return (
-    <div> Login </div>
-  );
+    const [email, setEmail] = useState<string>()
+    const [error, setError] = useState<string>('')
+    const onSubmit = () => {
+        // email
+        console.log(email)
+        if (!email) {
+            setError('Required');
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            setError('Invalid email address');
+        }
+    }
+
+    const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
+    return (
+        <div>
+            <h2>Forgot your password?</h2>
+            <div>
+                <input name={'email'} type={'email'} placeholder={'Email'} onChange={handleChangeEmail}/>
+                {error && <div style={{color: 'red'}}>{error}</div>}
+            </div>
+            <p>Enter your email address and we will send you further instructions</p>
+            <button onClick={onSubmit}>Send Instructions</button>
+            <p>Did you remember your password?</p>
+            <NavLink to = {'/login'} className={s.link}>Try logging in</NavLink>
+        </div>
+    );
 }
