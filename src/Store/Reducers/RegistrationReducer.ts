834b431dcfcf8fa8714f3fcfa7895953938f5135
vetero4ok <vetero4ok@gmail.com>
2021-09-18 21:40:23 +0300

    create case for SET-NEW-PASSWORD and dispatching TC

--- src/Store/Reducers/RegistrationReducer.ts
+++ src/Store/Reducers/RegistrationReducer.ts
@@ -1,50 +1,70 @@
-import { RegistrationFormType } from '../../Pages/Registration/Registration';
-import { registerApi } from '../../Dal/Api';
-import { Dispatch } from 'redux';
+import {RegistrationFormType} from '../../Pages/Registration/Registration';
+import {registerApi} from '../../Dal/Api';
+import {Dispatch} from 'redux';
 
 type RegistrationACType = {
-    newUser:adedUserType
-    initUser:boolean
-    type:'/REGISTRATION/NEW-USER'
+    newUser: adedUserType
+    initUser: boolean
+    type: '/REGISTRATION/NEW-USER'
 }
 type adedUserType = {
-    _id:string
-    email:string
-    rememberMe:boolean
-    isAdmin:boolean
+    _id: string
+    email: string
+    rememberMe: boolean
+    isAdmin: boolean
 }
 type InitStateType = {
     addedUser: adedUserType
-    initUser:boolean
+    initUser: boolean
 }
-const initialState:InitStateType = {
-        addedUser: {
-                _id: "",
-                email: "",
-                rememberMe: false,
-                isAdmin: false,
-            },
-        initUser:false
+const initialState: InitStateType = {
+    addedUser: {
+        _id: '',
+        email: '',
+        rememberMe: false,
+        isAdmin: false,
+    },
+    initUser: false
 
 }
 
 
-export const registrationReducer = (state = initialState, action: RegistrationACType):InitStateType => {
+export const registrationReducer = (state = initialState, action: RegistrationActionType): InitStateType => {
     switch (action.type) {
         case '/REGISTRATION/NEW-USER':
             return {...state, addedUser: action.newUser, initUser: action.initUser}
+        case '/REGISTRATION/SET-NEW-PASSWORD':
+            return {...state}
     }
     return state
 };
 
-const registrationAC = (newUser:adedUserType, initUser:boolean):RegistrationACType => ({type:'/REGISTRATION/NEW-USER',  newUser , initUser })
+const registrationAC = (newUser: adedUserType, initUser: boolean): RegistrationACType => ({
+    type: '/REGISTRATION/NEW-USER',
+    newUser,
+    initUser
+})
+const setNewPasswordAC = () => ({type: '/REGISTRATION/SET-NEW-PASSWORD'} as const)
 
-export const registrationTS = (data:RegistrationFormType) => (dispatch: Dispatch) => {
+export const registrationTS = (data: RegistrationFormType) => (dispatch: Dispatch) => {
     registerApi.register(data).then(res => {
-            dispatch(registrationAC(res.data.addedUser,true))
-    }).catch((e:string) => {
+        dispatch(registrationAC(res.data.addedUser, true))
+    }).catch((e: string) => {
         alert(e)
     })
 }
+export const setNewPasswordTC = ( password: string, resetPasswordToken: string ) => (dispatch: Dispatch) => {
+    registerApi.setNewPassword(password,resetPasswordToken)
+        .then((res) => {
+            dispatch(setNewPasswordAC())
+        })
+        .catch((e: string) => {
+            console.log(e)
+        })
 
 
+}
+export type RegistrationActionType =
+    | RegistrationACType
+    | SetNewPasswordAT
+export type SetNewPasswordAT = ReturnType<typeof setNewPasswordAC>
