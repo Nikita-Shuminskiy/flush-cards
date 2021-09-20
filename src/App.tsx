78092206d8c3d64377b67efbb7c41fcd3a06cf0e
Nick <shuminskiy.nik@mail.ru>
2021-09-20 21:15:17 +0300

    Fix problems with app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import {Login} from './Pages/Login/Login';
@@ -10,23 +10,45 @@ import Test from './Common/Test/Test';
 import NavBar from './Pages/NavBar/NavBar';
 import {Profile} from './Pages/Profile/Profile';
 import {CheckEmail} from './Pages/PasswordRecovery/CheckEmail/CheckEmail';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppRootStateType } from './Store/Store';
+import { AlertContentType, authMe, removeAlert } from './Store/Reducers/AppReducer';
+import { Alert } from './Common/Alert/Alert';
+import { InitialLayout } from './Common/InitialLayout/InitialLayout';
 
 
 function App() {
+    const dispatch = useDispatch()
+    const alertList = useSelector<AppRootStateType, AlertContentType[]>(state => state.app.alertList)
+    const initApp = useSelector<AppRootStateType, boolean>(state => state.app.initialApp)
+    const closeAlet = (id: number) => dispatch(removeAlert(id))
+
+    useEffect(() => {
+        dispatch(authMe())
+    }, [])
+
     return (
-        <div className="App">
-            <NavBar/>
-            <Switch>
-                <Route path={'/login'} render={() => <Login/>}/>
-                <Route path={'/registration'} render={() => <Registration/>}/>
-                <Route path={'/profile'} render={() => <Profile/>}/>
-                <Route path={'/404'} render={() => <GlobalError/>}/>
-                <Route path={'/recoverypassword'} render={() => <PasswordRecovery/>}/>
-                <Route path={'/set-new-password/:token'} render={() => <NewPassword/>}/>
-                <Route path={'/check-email'} render={() => <CheckEmail/>}/>
-                <Route path={'/test'} render={() => <Test/>}/>
-            </Switch>
-        </div>
+      <>
+          {
+              !initApp ? <InitialLayout/>
+                  :
+                  <div className="App">
+                      <NavBar/>
+                      <Switch>
+                          <Route path={'/login'} render={() => <Login/>}/>
+                          <Route path={'/registration'} render={() => <Registration/>}/>
+                          <Route path={'/profile'} render={() => <Profile/>}/>
+                          <Route path={'/404'} render={() => <GlobalError/>}/>
+                          <Route path={'/recoverypassword'} render={() => <PasswordRecovery/>}/>
+                          <Route path={'/set-new-password/:token'} render={() => <NewPassword/>}/>
+                          <Route path={'/check-email'} render={() => <CheckEmail/>}/>
+                          <Route path={'/test'} render={() => <Test/>}/>
+                      </Switch>
+
+                      <Alert alertList={alertList} onCloseAlert={closeAlet}/>
+                  </div>
+          }
+      </>
     );
 }
 
