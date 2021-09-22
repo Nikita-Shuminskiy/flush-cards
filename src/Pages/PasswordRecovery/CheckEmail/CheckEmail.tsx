1970a8ad2b2012e88e55523002ddbd365cdd6451
vetero4ok <vetero4ok@gmail.com>
2021-09-22 15:41:20 +0300

    fix layout for 'CheckEmail'

--- src/Pages/PasswordRecovery/CheckEmail/CheckEmail.tsx
+++ src/Pages/PasswordRecovery/CheckEmail/CheckEmail.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import envelope from '../../../Images/envelope.png'
 import {useSelector} from 'react-redux';
 import {AppRootStateType} from '../../../Store/Store';
+import s from './CheckEmail.module.css';
 
 
 export const CheckEmail = () => {
     const email = useSelector<AppRootStateType, string>(state => state.registration.email)
 
     return (
-        <div>
-            <img src={envelope} alt = {'envelope'}/>
-            <h2>Check Email</h2>
-            <p>We’ve sent an Email with instructions to {email}</p>
+        <div className={s.wrapContainer}>
+            <div className={s.container}>
+                <img src={envelope} alt={'envelope'}/>
+                <h2>Check Email</h2>
+                <p>We’ve sent an Email with instructions to {email}</p>
+            </div>
         </div>
     );
 }
