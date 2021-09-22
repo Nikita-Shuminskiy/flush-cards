4790af16bb0a44104ea98549ffc026c76ac2e2b6
vetero4ok <vetero4ok@gmail.com>
2021-09-22 15:22:20 +0300

    fix layout for 'Forgot your password?'

--- src/Pages/PasswordRecovery/PasswordRecovery.tsx
+++ src/Pages/PasswordRecovery/PasswordRecovery.tsx
@@ -4,6 +4,8 @@ import s from './PasswordRecovery.module.css'
 import {useDispatch, useSelector} from 'react-redux';
 import {recoveryPasswordTC} from '../../Store/Reducers/RegistrationReducer';
 import {AppRootStateType} from '../../Store/Store';
+import SuperButton from '../../Common/Test/c2-SuperButton/SuperButton';
+import SuperInputText from '../../Common/Test/c1-SuperInputText/SuperInputText';
 
 
 export const PasswordRecovery = () => {
@@ -27,16 +29,21 @@ export const PasswordRecovery = () => {
     }
     const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
     return (
-        <div>
-            <h2>Forgot your password?</h2>
-            <div>
-                <input name={'email'} type={'email'} placeholder={'Email'} onChange={handleChangeEmail}/>
-                {error && <div style={{color: 'red'}}>{error}</div>}
+        <div className={s.wrapContainer}>
+            <div className={s.container}>
+                <h2>Forgot your password?</h2>
+                <div>
+                    <SuperInputText name={'email'} type={'email'} placeholder={'Email'} onChange={handleChangeEmail}/>
+                    {error && <div style={{color: 'red'}}>{error}</div>}
+                </div>
+                <p>Enter your email address and we will send you further instructions</p>
+                <SuperButton onClick={onSubmit} value={'Send Instructions'}/>
+
+                <div>
+                    <p>Did you remember your password?</p>
+                    <NavLink to={'/login'} className={s.link}>Try logging in</NavLink>
+                </div>
             </div>
-            <p>Enter your email address and we will send you further instructions</p>
-            <button onClick={onSubmit}>Send Instructions</button>
-            <p>Did you remember your password?</p>
-            <NavLink to={'/login'} className={s.link}>Try logging in</NavLink>
         </div>
     );
 }
