f97f904a26e300cf88aebd274395153887d97aa5
vetero4ok <vetero4ok@gmail.com>
2021-09-22 20:32:19 +0300

    fix bug (it was not possible to update the new password)

--- src/Dal/Api.ts
+++ src/Dal/Api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import {RegistrationFormType} from '../Pages/Registration/Registration';
 
 const createInstance = axios.create({
-    //baseURL: 'https://neko-back.herokuapp.com/2.0',
-    baseURL: 'http://localhost:7542/2.0/',
+   baseURL: 'https://neko-back.herokuapp.com/2.0',
+   // baseURL: 'http://localhost:7542/2.0/',
     withCredentials: true
 })
 
--- src/Pages/NavBar/NavBar.tsx
+++ src/Pages/NavBar/NavBar.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import { NavLink, Redirect, useHistory } from 'react-router-dom';
+import {NavLink, Redirect, Route, useHistory} from 'react-router-dom';
 import s from './Navbar.module.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { AppRootStateType } from '../../Store/Store';
-import { isLogoutTC } from '../../Store/Reducers/LoginReducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from '../../Store/Store';
+import {isLogoutTC} from '../../Store/Reducers/LoginReducer';
 
 const NavBar = () => {
-    let {isLoggedIn} = useSelector<AppRootStateType,any>( state => state.login)
+    let {isLoggedIn} = useSelector<AppRootStateType, any>(state => state.login)
     const dispatch = useDispatch()
     const history = useHistory()
 
     const logoutHandler = () => {
         dispatch(isLogoutTC(false))
     }
-    if(!isLoggedIn) {
+
+    if ((!isLoggedIn) && (<Route path={'/set-new-password/'}/>)) {
         return <Redirect to={'/login'}/>
     }
     return (
         <div>
             <div className={s.nav}>
-                <div className={`${s.item}`}><NavLink  activeClassName={s.active} to={'/set-new-password'}>Entrynewpassword</NavLink></div>
-                <div className={s.item}><NavLink  activeClassName={s.active} to={'/test'}>test</NavLink></div>
-                <div className={s.item + ' ' + s.setting}><NavLink  activeClassName={s.active} to={'/profile'}>Profile</NavLink></div>
-                <div className={s.item + ' ' + s.setting}><NavLink  activeClassName={s.active} to={'/registration'}>Registration</NavLink></div>
-                <div className={s.item + ' ' + s.setting}><NavLink  activeClassName={s.active} to={'/login'}>Login</NavLink></div>
-                <div className={s.item + ' ' + s.setting}><NavLink  activeClassName={s.active} to={'/recoverypassword'}>passwordRecovery</NavLink></div>
-                {isLoggedIn && <button onClick={logoutHandler}>LOGOUT</button> }
+                <div className={s.item + ' ' + s.setting}><NavLink activeClassName={s.active}
+                                                                   to={'/profile'}>Profile</NavLink></div>
+                <div className={s.item}><NavLink activeClassName={s.active} to={'/test'}>test</NavLink></div>
+                <div className={s.item + ' ' + s.setting}><NavLink activeClassName={s.active}
+                                                                   to={'/registration'}>Registration</NavLink></div>
+                <div className={s.item + ' ' + s.setting}><NavLink activeClassName={s.active}
+                                                                   to={'/login'}>Login</NavLink></div>
+                <div className={s.item + ' ' + s.setting}><NavLink activeClassName={s.active}
+                                                                   to={'/recoverypassword'}>
+                    Password Recovery</NavLink></div>
+                <div className={`${s.item}`}><NavLink activeClassName={s.active} to={'/set-new-password'}>Entry New
+                    Password</NavLink></div>
+                {isLoggedIn && <button onClick={logoutHandler}>LOGOUT</button>}
             </div>
         </div>
     );
