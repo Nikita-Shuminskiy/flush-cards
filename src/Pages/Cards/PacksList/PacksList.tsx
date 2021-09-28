7b825fa504bcbd5bad1566ff7d574e1c7aafe957
vetero4ok <vetero4ok@gmail.com>
2021-09-28 15:45:04 +0300

    UI find by name

--- src/Pages/Cards/PacksList/PacksList.tsx
+++ src/Pages/Cards/PacksList/PacksList.tsx
@@ -1,18 +1,23 @@
 import SuperInputText from '../../../Common/Test/c1-SuperInputText/SuperInputText';
 import Table from '../../Table/Table';
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import SuperButton from '../../../Common/Test/c2-SuperButton/SuperButton';
 import s from './PacksList.module.css'
 
-export const PacksList = () => {
+export const PacksList = React.memo(() => {
+    const [findByName, setFindByName] = useState<string>('')
+
+    const handleChangeFindName = (e: ChangeEvent<HTMLInputElement>) => setFindByName(e.currentTarget.value)
+    const onSubmit = () => {
+        console.log(findByName)
+    }
     return (
         <div className={s.container}>
             <h3>Packs list</h3>
             <div className={s.header}>
-
-                <SuperInputText/>
-                <SuperInputText/>
-                <SuperButton value={'Add new pack'}/>
+                <SuperInputText name={'SearchByName'} placeholder={'Search by name'} onChange={handleChangeFindName}/>
+                {/*<SuperInputText/>*/}
+                <SuperButton value={'Add new pack'} onClick={onSubmit}/>
             </div>
             <div>
                 <Table/>
@@ -22,4 +27,4 @@ export const PacksList = () => {
             </div>
         </div>
     );
-}
+});
