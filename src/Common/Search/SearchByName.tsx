c4ff7962a9da16547b473f217dc69c0c2226f8df
vetero4ok <vetero4ok@gmail.com>
2021-09-29 16:41:30 +0300

    universal search by name

---
+++ src/Common/Search/SearchByName.tsx
@@ -0,0 +1,25 @@
+import SuperInputText from '../Test/c1-SuperInputText/SuperInputText';
+import React, {ChangeEvent, useState} from 'react';
+
+type SearchByNamePropsType = {
+    searchName: (name: string) => void
+}
+
+export const SearchByName = (props: SearchByNamePropsType) => {
+    const [findByName, setFindByName] = useState<string>('')
+    const handleChangeFindName = (e: ChangeEvent<HTMLInputElement>) => setFindByName(e.currentTarget.value)
+    const onEnterHandler = () => {
+        const validate = findByName.trim()
+        props.searchName(validate)
+        // dispatch(searchNameTC(validate))
+        setFindByName('')
+    }
+    return (
+        <>
+            <SuperInputText name={'SearchByName'} placeholder={'Search name (onEnter)'}
+                            onEnter={onEnterHandler} onChange={handleChangeFindName}
+                            style={{width: '400px'}}
+            />
+        </>
+    );
+}
--- src/Pages/Deck/PacksList/PacksList.tsx
+++ src/Pages/Deck/PacksList/PacksList.tsx
@@ -1,25 +1,19 @@
-import SuperInputText from '../../../Common/Test/c1-SuperInputText/SuperInputText';
 import Table from '../../Table/Table';
-import React, {ChangeEvent, useState} from 'react';
+import React, {useState} from 'react';
 import SuperButton from '../../../Common/Test/c2-SuperButton/SuperButton';
 import s from './PacksList.module.css'
 import {useDispatch} from 'react-redux';
 import {searchNameTC} from '../../../Store/Reducers/DeckReducer';
-import TableMenu from "../../Table/ComponentsTable/TableMenu";
+import TableMenu from '../../Table/ComponentsTable/TableMenu';
+import {SearchByName} from '../../../Common/Search/SearchByName';
 
 export const PacksList = React.memo(() => {
     const [status, setStatus] = useState<boolean>(false)
     const clearMenu = () => {
         setStatus(false)
     }
-    const [findByName, setFindByName] = useState<string>('')
     const dispatch = useDispatch()
-    const handleChangeFindName = (e: ChangeEvent<HTMLInputElement>) => setFindByName(e.currentTarget.value)
-    const onEnterHandler = () => {
-        const validate = findByName.trim()
-        dispatch(searchNameTC(validate))
-        setFindByName('')
-    }
+      const handlerSearchName = (name:string) => dispatch(searchNameTC(name))
     const onSubmit = () => {
         setStatus(true)
     }
@@ -28,10 +22,7 @@ export const PacksList = React.memo(() => {
         <div className={s.container}>
             <h3>Packs list</h3>
             <div className={s.header}>
-                <SuperInputText name={'SearchByName'} placeholder={'Search by name (onEnter)'}
-                                onEnter={onEnterHandler} onChange={handleChangeFindName}
-                                style={{width:'400px'}}
-                />
+                <SearchByName searchName={handlerSearchName}/>
                 <SuperButton value={'Add new pack'} onClick={onSubmit}/>
             </div>
             <div>
